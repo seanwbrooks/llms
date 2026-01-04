cf6a519947005b900b75daa872f2e58ecf1748ed
Sean Brooks <sean@Seans-MacBook-Pro.local>
2026-01-04 15:09:23 -0500

    trained model in pytorch-tensor.py and saved 100% accurate model in model.pth for later use

--- pytorch-tensor.py
+++ pytorch-tensor.py
@@ -72,3 +72,107 @@ print('Total number of trainable model parameters: ', num_params)
 print('Weight parameter ', model.layers[0].weight)
 print('Dimensions of weight using .shape ', model.layers[0].weight.shape)
 
+torch.manual_seed(123)
+X = torch.rand((1,50))
+out = model(X)
+print(out)
+
+with torch.no_grad():
+	out = model(X)
+print(out)
+
+with torch.no_grad():
+	out = torch.softmax(model(X), dim=1)
+print(out)
+
+## Setting up efficient data loaders
+# Creating a small toy dataset
+X_train = torch.tensor([[-1.2, 3.1],[-0.9, 2.9],[-0.5, 2.6],[2.3, -1.1],[2.7, -1.5]])
+y_train = torch.tensor([0, 0, 0, 1, 1])
+X_test = torch.tensor([[-0.8, 2.8],[2.6, -1.6]])
+y_test = torch.tensor([0, 1])
+
+# Defining a custom Dataset class
+from torch.utils.data import Dataset
+
+class ToyDataset(Dataset):
+	def __init__(self, X, y):
+		self.features = X
+		self.labels = y
+	def __getitem__(self, index):
+		one_x = self.features[index]
+		one_y = self.labels[index]
+		return one_x, one_y
+	def __len__(self):
+		return self.labels.shape[0]
+
+train_ds = ToyDataset(X_train, y_train)
+test_ds = ToyDataset(X_test, y_test)
+
+print('Length of train dataset ', len(train_ds))
+
+# Instantiating data loaders
+from torch.utils.data import DataLoader
+
+torch.manual_seed(123)
+
+train_loader = DataLoader(dataset=train_ds, batch_size=2, shuffle=True, num_workers=0)
+
+test_loader = DataLoader(dataset=test_ds, batch_size=2, shuffle=False, num_workers=0)
+
+for idx, (x, y) in enumerate(train_loader):
+	print(f"Batch {idx+1}: ", x, y)
+
+# A training loader that drops the last batch
+train_loader = DataLoader(dataset=train_ds, batch_size=2, shuffle=True, num_workers=0, drop_last=True)
+
+for idx, (x, y) in enumerate(train_loader):
+	print(f"Batch {idx+1}: ", x, y)
+
+# Neural Network training in PyTorch
+torch.manual_seed(123)
+
+model = NeuralNetwork(num_inputs=2, num_outputs=2)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
+
+num_epochs = 3
+for epoch in range(num_epochs):
+	model.train()
+
+	for batch_idx, (features, labels) in enumerate(train_loader):
+		logits = model(features)
+		
+		loss = F.cross_entropy(logits, labels)
+	
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+
+		### Logging
+		print(
+			f"Epoch: {epoch+1:03d}/{num_epochs:03d}"
+			f" | Batch: {batch_idx:03d}/{len(train_loader):03d}"
+			f" | Train Loss: {loss:.2f}"
+		)
+	model.eval()
+
+# A function to compute the pediction accuracy
+def compute_accuracy(model, dataloader):
+	model = model.eval()
+	correct = 0.0
+	total_examples = 0
+
+	for idx, (features, labels) in enumerate(dataloader):
+		with torch.no_grad():
+			logits = model(features)
+
+		predictions = torch.argmax(logits, dim=1)
+		compare = labels == predictions
+		correct += torch.sum(compare)
+		total_examples += len(compare)
+
+	return (correct / total_examples).item()
+
+print('Compute accuracy ', compute_accuracy(model, train_loader))
+
+torch.save(model.state_dict(), "model.pth")
