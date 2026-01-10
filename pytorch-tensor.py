ddf2d40585c1267c70083e2b8219cdcaa594bfb7
Sean Brooks <sean@Seans-MacBook-Pro.local>
2026-01-10 10:12:21 -0500

    training a loop on silicon gpu

--- pytorch-tensor.py
+++ pytorch-tensor.py
@@ -181,3 +181,34 @@ torch.save(model.state_dict(), "model.pth")
 print(torch.cuda.is_available())
 device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 print(device)
+
+# A training loop on a GPU
+torch.manual_seed(123)
+model = NeuralNetwork(num_inputs=2, num_outputs=2)
+
+device = torch.device("mps")
+model = model.to(device)
+
+optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
+
+num_epochs = 3
+
+for epoch in range(num_epochs):
+	
+	model.train()
+	for batch_idx, (features, labels) in enumerate(train_loader):
+		features, labels = features.to(device), labels.to(device)
+		logits = model(features)
+		loss = F.cross_entropy(logits, labels) # Loss function
+
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+
+		### LOGGING
+		print(f"Epoch: {epoch+1:03d}/{num_epochs:03d}"
+		      f" | Batch {batch_idx:03d}/{len(train_loader):03d}"
+		      f" | Train/Val Loss: {loss:.2f}")
+
+		model.eval()
+
