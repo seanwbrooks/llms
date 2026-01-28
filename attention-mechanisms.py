10ae018537272d79c34bd94778d249e7eb3be0c5
Sean Brooks <sean@Seans-MacBook-Pro.local>
2026-01-27 20:00:50 -0500

    casual attention class

--- attention-mechanisms.py
+++ attention-mechanisms.py
@@ -95,3 +95,39 @@ class SelfAttention_v2(nn.Module):
 torch.manual_seed(789)
 sa_v2 = SelfAttention_v2(d_in, d_out)
 print(sa_v2(inputs))
+
+batch = torch.stack((inputs, inputs), dim=0)
+print(batch)
+
+torch.Size([2, 6, 3])
+
+# A compact casual attention class
+class CasualAttention(nn.Module):
+	def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
+		super().__init__()
+		self.d_out = d_out
+		self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+		self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+		self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+		self.dropout = nn.Dropout(dropout)
+		self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1))
+
+	def forward(self, x):
+		b, num_tokens, d_in = x.shape
+		keys = self.W_key(x)
+		queries = self.W_query(x)
+		values = self.W_value(x)
+		
+		attn_scores = queries @ keys.transpose(1, 2)
+		attn_scores.masked_fill_(self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+		attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+		attn_weights = self.dropout(attn_weights)
+
+		context_vec = attn_weights @ values
+		return context_vec
+
+torch.manual_seed(123)
+context_length = batch.shape[1]
+ca = CasualAttention(d_in, d_out, context_length, 0.0)
+context_vecs = ca(batch)
+print("context_vecs.shape: ", context_vecs.shape)
