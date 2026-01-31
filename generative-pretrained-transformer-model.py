5b81aff935eba0ff561d55c7872326a6775b1fe5
Sean Brooks <swbrooks87@gmail.com>
2026-01-31 16:03:36 -0500

    guassian activation that has smoother activation than linear module, matplotlib to compare and show difference

--- generative-pretrained-transformer-model.py
+++ generative-pretrained-transformer-model.py
@@ -113,3 +113,28 @@ var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
 print("Mean:\n", mean)
 print("Variance:\n", var)
 
+# Implementing GELU (Iaussian error linear unit) activation function
+class GELU(nn.Module):
+	def __init__(self):
+		super().__init__()
+	def forward(self, x):
+		return 0.5 * x * (1 + torch.tanh(
+			torch.sqrt(torch.tensor(2.0 / torch.pi)) *
+			(x + 0.044715 * torch.pow(x, 3))
+		))
+# showing te smooth activation guassian v linear
+import matplotlib.pyplot as plt
+gelu, relu = GELU(), nn.ReLU()
+
+x = torch.linspace(-3, 3, 100)
+y_gelu, y_relu = gelu(x), relu(x)
+plt.figure(figsize=(8,3))
+for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "RELU"]), 1):
+	plt.subplot(1, 2, i)
+	plt.plot(x, y)
+	plt.title(f"{label} activation function")
+	plt.xlabel("x")
+	plt.ylabel(f"{label}(x)")
+	plt.grid(True)
+plt.tight_layout()
+plt.show()
