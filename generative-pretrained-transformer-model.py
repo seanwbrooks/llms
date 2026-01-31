d89637c8de28bc09afda01ffce7e8bb9d98dc108
Sean Brooks <swbrooks87@gmail.com>
2026-01-30 19:10:53 -0500

    layer normalization

--- generative-pretrained-transformer-model.py
+++ generative-pretrained-transformer-model.py
@@ -83,3 +83,33 @@ mean = out.mean(dim=-1, keepdim=True)
 var = out.var(dim=-1, keepdim=True)
 print("Mean: \n", mean)
 print("Variance: \n", var)
+
+out_norm = (out - mean) / torch.sqrt(var)
+mean = out_norm.mean(dim=-1, keepdim=True)
+var = out_norm.var(dim=-1, keepdim=True)
+print("Normalized layer outputs: \n", out_norm)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+# A layer normalization class
+class LayerNorm(nn.Module):
+	def __init__(self, emb_dim):
+		super().__init__()
+		self.eps = 1e-5
+		self.scale = nn.Parameter(torch.ones(emb_dim))
+		self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+	def forward(self, x):
+		mean = x.mean(dim=-1, keepdim=True)
+		var = x.var(dim=-1, keepdim=True, unbiased=False)
+		norm_x = (x - mean) / torch.sqrt(var + self.eps)
+		return self.scale * norm_x + self.shift
+
+torch.set_printoptions(sci_mode=False)
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
